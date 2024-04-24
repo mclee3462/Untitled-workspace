432ea602944c691996bfd7fd86e5b1ad044b6cd8
mclee3462 <miniman3462@byui.edu>
2024-04-23 18:33:59 -0600

    new file:   python/favorite.py 	new file:   python/madlib.py

---
+++ python/favorite.py
@@ -0,0 +1,8 @@
+favorite_color = input("Please type your favorite color: ")
+favorite_season = input("What is your favorite season? ")
+favorite_food = input("What is your favorite food? ")
+
+
+print("\nYour favorite color is", favorite_color)
+print("Your favorite season is", favorite_season)
+print("Your favorite food is", favorite_food)
---
+++ python/madlib.py
@@ -0,0 +1,14 @@
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb1 = input("verb: ")
+exclamation = input("exclamation: ").capitalize()
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+
+
+print("\nYour story is:\n")
+print(f"The other day, I was really in trouble. It all started when I saw a very")
+print(f"{adjective} {animal} {verb1} down the hallway. \"{exclamation}\" I yelled. But all")
+print(f"I could think to do was to {verb2} over and over. Miraculously,")
+print(f"that caused it to stop, but not before it tried to {verb3}")
+print("right in front of my family.")
