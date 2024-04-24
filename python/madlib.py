11a385351db55f8f83ae5f1554af02617fdbcdc7
mclee3462 <miniman3462@byui.edu>
2024-04-24 15:58:59 -0600

    modified:   python/madlib.py

--- python/madlib.py
+++ python/madlib.py
@@ -1,14 +1,15 @@
-adjective = input("adjective: ")
+adjective1 = input("adjective: ")
 animal = input("animal: ")
 verb1 = input("verb: ")
 exclamation = input("exclamation: ").capitalize()
 verb2 = input("verb: ")
+adjective2 = input("adjective: ")
 verb3 = input("verb: ")
 
 
 print("\nYour story is:\n")
 print(f"The other day, I was really in trouble. It all started when I saw a very")
-print(f"{adjective} {animal} {verb1} down the hallway. \"{exclamation}\" I yelled. But all")
-print(f"I could think to do was to {verb2} over and over. Miraculously,")
+print(f"{adjective1} {animal} {verb1} down the hallway. \"{exclamation}\" I yelled. But all")
+print(f"I could think to do was to {verb2} over and over. {adjective2},")
 print(f"that caused it to stop, but not before it tried to {verb3}")
 print("right in front of my family.")
